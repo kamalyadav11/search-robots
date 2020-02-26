464eabaf38b5a6991022fb41e45833f074491ea3
kamal11 <kyzx11@gmail.com>
2020-02-26 19:23:19 +0530

    Updates

---
+++ precache-manifest.68944514c33f7d5515a2e5b3509bf3ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536e4ecb1a641cdbd80d92733cb06011",
+    "url": "/kamalyadav11/search-robots/index.html"
+  },
+  {
+    "revision": "0bf197e63ae3a3d3a01b",
+    "url": "/kamalyadav11/search-robots/static/css/main.4fe8830b.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/kamalyadav11/search-robots/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kamalyadav11/search-robots/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf197e63ae3a3d3a01b",
+    "url": "/kamalyadav11/search-robots/static/js/main.e2e239c7.chunk.js"
+  },
+  {
+    "revision": "e58574b319394ca1425e",
+    "url": "/kamalyadav11/search-robots/static/js/runtime-main.69169f4e.js"
+  }
+]);
